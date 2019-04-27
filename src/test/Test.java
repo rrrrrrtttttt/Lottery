7e1a6f40ae17c3060ac2f7dd79328b29258e1fb6
trp <trp@trp-GL552JX>
2019-04-27 16:21:31 +0800

    000

---
+++ src/test/Test.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Client Start");
+		Client c = new Client();
+		c.start();
+	}
+
+}
